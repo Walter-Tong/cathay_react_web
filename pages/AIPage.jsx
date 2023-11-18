f9bc4c1c507c6df395c4ae9f3cc64e173226546b
Walter-Tong <walter19@connect.hku.hk>
2023-11-19 05:51:08 +0800

    ken

---
+++ pages/AIPage.jsx
@@ -0,0 +1,11 @@
+import LLMChatBoxes from "../src/components/LLMChatBoxes";
+
+function AIPage() {
+    return (
+        <div>
+            <LLMChatBoxes />
+        </div>
+    )
+}
+
+export default AIPage
