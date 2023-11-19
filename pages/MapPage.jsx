9cbfb7406b1486163563a0c5aa781c58d14c38ec
Kenneth Kwan <123370597+kennethkn@users.noreply.github.com>
2023-11-19 10:47:20 +0800

    Update MapPage.jsx

--- pages/MapPage.jsx
+++ pages/MapPage.jsx
@@ -1,7 +1,7 @@
 import Show360Image from '../src/components/Show360Image'
 
 function MapPage() {
-  return <Show360Image path="../public/images/IMG_2556.jpeg" />
+  return <Show360Image path="images/IMG_2556.jpeg" />
 }
 
 export default MapPage
